a0f4abd5c503f6e908dc89421caae39cc768ca2c
stark7991 <135486714+StarkAshley-FS@users.noreply.github.com>
2024-11-02 21:36:37 -0400

    connecting mongoose database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
 dotenv.config();
+connectDB();
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => res.send('Spotify Music Search App'));
 
