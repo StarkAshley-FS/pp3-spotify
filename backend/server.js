9ee11c8e3c5c070a1c10f70450ac042acbcd86ab
stark7991 <135486714+StarkAshley-FS@users.noreply.github.com>
2024-11-04 13:10:01 -0500

    fixing endpoint

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use('/api', authRoutes);
 
 app.get('/', (req, res) => res.send('Spotify Music Search App'));
 
