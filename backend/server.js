620e2d81261ecc8732816d18b002c3db1ffc733a
stark7991 <135486714+StarkAshley-FS@users.noreply.github.com>
2024-11-06 12:27:19 -0500

    adding spotify routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const spotifyRoutes = require('./routes/spotifyRoutes')
 
 dotenv.config();
 connectDB();
@@ -12,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', authRoutes);
+app.use('/api', authRoutes, spotifyRoutes);
 
 app.get('/', (req, res) => res.send('Spotify Music Search App'));
 
