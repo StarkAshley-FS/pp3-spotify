22decd424419d3536b3f2ad868426ffee1880b48
stark7991 <135486714+StarkAshley-FS@users.noreply.github.com>
2024-11-18 18:59:29 -0500

    adding pages to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import LoginCallback from './pages/LoginCallback';
+import SearchPage from './pages/SearchPage';
+import Navbar from './components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/callback" element={<LoginCallback />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
