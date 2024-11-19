7553149cb7d8727aa0bef5dc66c478136ef69f3e
Ashley Stark-Padua <135486714+StarkAshley-FS@users.noreply.github.com>
2024-11-18 20:11:23 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,14 @@ app.use(cookieParser());
 
 app.use('/api', authRoutes, spotifyRoutes);
 
-app.get('/', (req, res) => res.send('Spotify Music Search App'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => res.send('Spotify Music Search App'));
+}
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
