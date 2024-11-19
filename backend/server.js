521968feaee59c731a6ca7be40e6701e8c072cec
stark7991 <135486714+StarkAshley-FS@users.noreply.github.com>
2024-11-18 19:54:08 -0500

    heroku debug

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const spotifyRoutes = require('./routes/spotifyRoutes');
 const cookieParser = require('cookie-parser');
-const path = require('path');
 
 dotenv.config();
 connectDB();
@@ -19,13 +18,7 @@ app.use(cookieParser());
 
 app.use('/api', authRoutes, spotifyRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'frontend/build')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
-    });
-  } else {
-    app.get('/', (req, res) => res.send('Spotify Music Search App'));
-  }
+app.get('/', (req, res) => res.send('Spotify Music Search App'));
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
